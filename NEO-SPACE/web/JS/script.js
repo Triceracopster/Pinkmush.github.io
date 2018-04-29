0df411a45d1e7ec7908dfa33fd718af6c5fba83b
Triceracopster <36891742+Triceracopster@users.noreply.github.com>
2018-04-29 14:48:03 +0300

    Add files via upload

--- NEO-SPACE/web/JS/script.js
+++ NEO-SPACE/web/JS/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     $('.slider2').slick({
+        lazyLoad: 'progressive',
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 7000,
-        speed: 300
+        speed: 500
     });
 });
