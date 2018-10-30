5e3b34614ce67b7f2c63836031d4819877f6830c
Triceracopster <36891742+Triceracopster@users.noreply.github.com>
2018-10-30 07:27:06 +0300

    Add files via upload

--- RomanIsaev/web/js/script.js
+++ RomanIsaev/web/js/script.js
@@ -6,12 +6,13 @@ $(document).ready(function () {
         infinite: true,
         speed: 350,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 6000,
         slidesToShow: 3,
         cssEase: 'linear',
         appendArrows: $('.arrows1'),
-        prevArrow: '<button id="prev" type="button" class="btn btn-outline-secondary btnLeft" aria-pressed="false" autocomplete="on"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
-        nextArrow: '<button id="next" type="button" class="btn btn-outline-secondary btnRight" aria-pressed="false" autocomplete="on"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
+        prevArrow: '<button id="prev" type="button" class="btn btn-outline-secondary btnLeft" aria-pressed="false" autocomplete="on"><i class="fa fa-chevron-left fa-2x" aria-hidden="true"></i></button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-outline-secondary btnRight" aria-pressed="false" autocomplete="on"><i class="fa fa-chevron-right fa-2x" aria-hidden="true"></i></button>',
+        pauseOnHover: true,
         responsive: [
             {
                 breakpoint: 1200,
@@ -24,15 +25,16 @@ $(document).ready(function () {
     });
     $('.slider2').slick({
         infinite: true,
-        speed: 500,
+        speed: 350,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 7000,
         slidesToShow: 2,
         cssEase: 'linear',
         appendArrows: $('.arrows2'),
-        prevArrow: '<button id="prev" type="button" class="btn btn-outline-secondary btnSlider2 btnLeft" aria-pressed="false" autocomplete="on"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
-        nextArrow: '<button id="next" type="button" class="btn btn-outline-secondary btnSlider2 btnRight" aria-pressed="false" autocomplete="on"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
-
+        prevArrow: '<button id="prev" type="button" class="btn btn-outline-secondary btnSlider2 btnLeft" aria-pressed="false" autocomplete="on"><i class="fa fa-chevron-left fa-2x" aria-hidden="true"></i></button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-outline-secondary btnSlider2 btnRight" aria-pressed="false" autocomplete="on"><i class="fa fa-chevron-right fa-2x" aria-hidden="true"></i></button>',
+        pauseOnHover: true,
+        pauseOnFocus: true,
         responsive: [
             {
                 breakpoint: 1200,
@@ -45,13 +47,14 @@ $(document).ready(function () {
     });
     $('.slider3').slick({
         infinite: true,
-        speed: 500,
+        speed: 350,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 4000,
         slidesToShow: 3,
         appendArrows: $('.arrows3'),
-        prevArrow: '<button id="prev" type="button" class="btn btn-outline-secondary btnLeft" aria-pressed="false" autocomplete="on"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
-        nextArrow: '<button id="next" type="button" class="btn btn-outline-secondary btnRight" aria-pressed="false" autocomplete="on"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
+        prevArrow: '<button id="prev" type="button" class="btn btn-outline-secondary btnLeft" aria-pressed="false" autocomplete="on"><i class="fa fa-chevron-left fa-2x" aria-hidden="true"></i></button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-outline-secondary btnRight" aria-pressed="false" autocomplete="on"><i class="fa fa-chevron-right fa-2x" aria-hidden="true"></i></button>',
+        pauseOnHover: true,
         responsive: [
             {
                 breakpoint: 850,
@@ -65,5 +68,5 @@ $(document).ready(function () {
 });
 
 jQuery(document).ready(function () {
-    $("div.dropdown-menu a").mPageScroll2id();
+    $("a.nav-link").mPageScroll2id();
 });
