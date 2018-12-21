7d4394e1972a45eece707b706a70d8dd501a5716
Triceracopster <Tolyagav@ya.ru>
2018-12-21 20:04:15 +0300

    Merge branch 'master' of C:\Users\Tolya\Documents\projects with conflicts.

---
+++ johnny/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".food-gallery-item a").on("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        return false;
+    });
+});
