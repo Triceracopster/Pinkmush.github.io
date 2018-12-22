71f4734a7a6ebd014a0186b836c4bc7ac5d16201
Triceracopster <Tolyagav@ya.ru>
2018-12-22 12:58:50 +0300

    Различные багфиксы, переделана галерея, доработано меню.

--- johnny/js/script.js
+++ johnny/js/script.js
@@ -1,10 +1,18 @@
-$(document).ready(function(){
-    $(".food-gallery-item a").on("click", function(e){
-        var anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $(anchor.attr('href')).offset().top
-        }, 777);
-        e.preventDefault();
-        return false;
+$(document).ready(function () {
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        infinite: true,
+        speed: 400,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        cssEase: 'linear',
+        pauseOnHover: true,
+        arrows: true,
+        appendArrows: $('.arrows1'),
+        prevArrow: '<button id="prev" type="button" class="btn btn-outline-secondary btnSlider2 btnLeft" aria-pressed="false" autocomplete="on"><i class="fa fa-3x fa-chevron-left" aria-hidden="true"></i></button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-outline-secondary btnSlider2 btnRight" aria-pressed="false" autocomplete="on"><i class="fa fa-3x fa-chevron-right" aria-hidden="true"></i></button>',
     });
 });
